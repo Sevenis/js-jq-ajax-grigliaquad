e59ad89f81b6da6a6ab1369ba3190e500c7b2263
Pietro Sette <pietrosette@gmail.com>
2020-08-27 22:09:12 +0200

    jquery + ajax done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
-
-    for(var i=0; i<6; i++){
+    // genero le square
+    for(var i=0; i<36; i++){
         $('.wrapper').append('<div class="square"></div>');
     }
 
+    //implemento cosa accade al click del mouse su una delle square
+    $('.square').click(function() {
+        var box = $(this);
 
+        $.ajax(
+                {
+                    //recupero l'api che genera il numero random
+                    url: "https://flynn.boolean.careers/exercises/api/random/int",
+                    method: "GET",
+                    success: function (risposta) {
+                        box.text(risposta.response);
+                        //elaboro il risultato ottenuto assegnando una classe colore in base al valore
+                        if (risposta.response < 6) {
+                            $(box).addClass("yellow");
+                        } else {
+                            $(box).addClass("green");
+                        }
+                    },
+                    error: function (richiesta, stato, errori) {
+                        alert("E' avvenuto un errore. " + errore);
+                    }
+                }
+            );
+    });
 
 });
-
-//** FUNZIONI
